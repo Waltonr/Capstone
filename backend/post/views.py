ecceede3c4c01e9296d55d46b7eac970fe8b8d1a
Waltonr <waltonr18126@gmail.com>
2022-05-26 16:00:29 -0400

    feat: Updated Project URLs File + Registered Post App in Project Settings + Created GET View Function for Post App + Created Post App URLs File

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_all_posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
