7be3b4112e70277614ef55c9decc60f410832b6b
Waltonr <waltonr18126@gmail.com>
2022-05-27 17:02:24 -0400

    feat: Created Recommendations App + Created Recommendation Model for Recommendations App + Registered Recommendations App in Project Settings + Updated Project URLs

---
+++ backend/recommendations/models.py
@@ -0,0 +1,11 @@
+from tkinter import CASCADE
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+
+class Recommendation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+    housing = models.CharField(max_length=255)
+
