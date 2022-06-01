74f759506196d4a4819f83b2374447cf8038c81e
Waltonr <waltonr18126@gmail.com>
2022-06-01 12:05:20 -0400

    feat: Imported CreatePost and CreateReply Components in SocialfeedPage

--- frontend/src/pages/SocialfeedPage/SocialfeedPage.js
+++ frontend/src/pages/SocialfeedPage/SocialfeedPage.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import DisplayPosts from "../../components/DisplayPost/DisplayPost";
+import CreatePost from "../../components/CreatePost/CreatePost";
+import DisplayReplies from "../../components/DisplayReplies/DisplayReplies";
+import CreateReplies from "../../components/CreateReply/CreateReply";
 
 const SocialfeedPage = (props) => {
     const [posts, setAllPosts] = useState()
+    const [replies, setAllReplies] = useState()
     const [user, token] = useAuth()
 
     useEffect(() => {
@@ -21,15 +25,31 @@ const SocialfeedPage = (props) => {
                 console.log("error with get all posts")
             }
         };
+        const getReplies = async() => {
+            try {
+                let response = await axios.get("http://127.0.0.1:8000/api/replies/all/", {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    },
+                });
+                console.log(response.data)
+                setAllReplies(response.data)
+            } catch (error) {
+                console.log("error with get all replies")
+            }
+        };
         getPosts();
+        getReplies();
     }, [token])
 
 
     return ( 
         <div>
             <h2>SOCIAL FEED</h2>
+            <CreatePost />
             <DisplayPosts getAllPostsProperty={posts} />
-            
+            <CreateReplies />
+            <DisplayReplies getAllRepliesProperty={replies} />
         </div>
      );
 }
