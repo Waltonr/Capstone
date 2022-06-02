a1c90bc48d528df4aee41bb293bc5944d83ab523
Waltonr <waltonr18126@gmail.com>
2022-06-02 10:41:03 -0400

    Modified Post Component for Edit Button to have Better Functionality

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
 import CreateReply from "../CreateReply/CreateReply";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import useCustomForm from "../../hooks/useCustomForm";
 
 
+let initvalues = {
+  text: ""
+}
 const Post = (props) => {
     const { post} = props
 
     const [user, token] = useAuth() 
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(initvalues, editPost)
     const [likedButton, setLikedButton] = useState("inactive");
     const [dislikedButton, setDislikedButton] = useState("inactive");
 
-  
+    async function editPost() {
+      try {
+        let response = await axios.put("http://127.0.0.1:8000/api/post/1/",
+        {
+            headers: {
+              Authorization: "Bearer " + token
+            }
+        }
+        );
+      } catch (error) {
+        console.log("error with editing post")
+      }
+    }
+
+    function updatePost() {
+    }
 
 
     function handleClick() {
@@ -25,12 +47,13 @@ const Post = (props) => {
       }
     return ( 
       <div>
-        {user.username}
+        <Link to="profile">{user.username}</Link>
         {post.text}
         <button className={likedButton} onClick={handleClick}></button>
         {post.likes}
         <button className={dislikedButton} onClick={handleClick}></button>
         {post.dislikes}
+        <button onClick={updatePost}>Edit</button>
         <CreateReply />
       </div>
      );
