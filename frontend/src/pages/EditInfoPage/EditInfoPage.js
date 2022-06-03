5b5daf6746c5727390197f0f0c3122a3a9a26054
Waltonr <waltonr18126@gmail.com>
2022-06-03 16:40:08 -0400

    Changed the Button Color in NavBar css File + Created EditInfo Page + Added an AXIOS PUT Request in EditInfo Page

---
+++ frontend/src/pages/EditInfoPage/EditInfoPage.js
@@ -0,0 +1,59 @@
+import React from "react"
+import axios from "axios"
+
+let initvalues = {
+    age: 0,
+    about: ""
+}
+
+const EditInfo = (props) => {
+    const [user, token] = useAuth()
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(initvalues, editInformation)
+
+    async function editInformation() {
+        try {
+            let response = await axios.put("http://127.0.0.1:8000/api/information/1",
+            formData,
+            {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            }
+            );
+        } catch (error) {
+            console.log("error with editing info")
+            
+        }
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    Information
+                </label>
+                <label>
+                    Age:{""}
+                    <input
+                     type="text" 
+                     name="age" 
+                     value={formData.age}
+                     onChange={handleInputChange}
+                     />    
+                </label>
+                <label>
+                    About:{""}
+                    <input
+                     type="text" 
+                     name="about" 
+                     value={formData.about}
+                     onChange={handleInputChange}
+                     />    
+                </label>
+            </div>
+            <button type='submit'>Submit</button>
+        </form>
+     );
+}
+ 
+export default EditInfo;
