f7eb015954783b6500f8872e15283af2c05b8af1
Waltonr <waltonr18126@gmail.com>
2022-06-03 11:27:14 -0400

    Modified Location Model + Created Housing Model and Serializer for Housing App

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from authentication.models import User
 
 # Create your models here.
 
 class Location(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
     continent = models.CharField(max_length=255)
--- backend/locations/serializers.py
+++ backend/locations/serializers.py
@@ -5,5 +5,5 @@ from .models import Location
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ['id', 'name', 'country', 'continent', 'user_id']
+        fields = ['id', 'name', 'country', 'continent']
         depth = 1
