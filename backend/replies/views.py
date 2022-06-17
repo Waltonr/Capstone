959d4b4dbb6f24617cf255417f5573ddf365d891
Waltonr <waltonr18126@gmail.com>
2022-06-17 18:01:22 -0400

    Added GET By Id View Function in Replies

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -9,7 +9,7 @@ from post.models import Post
 
 # Create your views here.
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 @permission_classes([IsAuthenticated])
 def user_reply(request, pk):
     print('User', f'{request.data} {request.user.email} {request.user.username}')
@@ -20,6 +20,10 @@ def user_reply(request, pk):
             serializer.save(user_id=request.user.id, post_id=post.id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        replies = Reply.objects.filter(post_id=pk)
+        serializer = ReplySerializer(replies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
 
 @api_view(['Get'])
