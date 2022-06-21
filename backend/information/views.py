1db1befe63e09339c0d2f1cc57bffc9ce3fb311e
Waltonr <waltonr18126@gmail.com>
2022-06-21 13:10:30 -0400

    Added GET By Id View Function in Information App

--- backend/information/views.py
+++ backend/information/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Information
 from .serializers import InformationSerializer
+User = get_user_model()
 
 # Create your views here.
 
@@ -26,13 +28,16 @@ def user_information(request):
         return Response(serializer.data)
 
 
-@api_view(['PUT'])
+@api_view(['PUT', 'GET'])
 @permission_classes([IsAuthenticated])
 def edit_information(request, pk):
+    info = get_object_or_404(Information, pk=pk)
     if request.method == 'PUT':
-        info = get_object_or_404(Information, pk=pk)
         serializer = InformationSerializer(info, data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(info=request.data)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        serializer = InformationSerializer(info)
+        return Response(serializer.data, status=status.HTTP_200_OK)
